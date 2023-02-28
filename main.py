c61994fd56dcbd93a1f003f1a5220ac1525cbc65
Truong Giang Do <01165758@pw.edu.pl>
2023-02-28 19:39:35 +0100

    Go straight

--- main.py
+++ main.py
@@ -9,10 +9,16 @@ cs2 = ColorSensor(INPUT_2)
 tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
 
 
-tank_drive.on_for_seconds(SpeedPercent(-30), SpeedPercent(-30), 3)
+while True:
+    # print(cs1.raw)
+    # print(cs2.raw)
+    # tank_drive.on_for_seconds(SpeedPercent(60), SpeedPercent(30), 3)
+    avg_left = sum(cs1.raw) / 3
+    avg_right = sum(cs2.raw) / 3
+    # print(avg_left, avg_right)
+    if avg_right > avg_left:
+        tank_drive.on_for_seconds(SpeedPercent(-10), SpeedPercent(-15), 0.5)
+    else:
+        tank_drive.on_for_seconds(SpeedPercent(-15), SpeedPercent(-10), 0.5)
 
-# while True:
-#     # print(cs1.raw)
-#     # print(cs2.raw)
-#     tank_drive.on_for_seconds(SpeedPercent(60), SpeedPercent(30), 3)
-#     sleep(3)
+    sleep(0.5)
