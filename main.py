0b8b8ae63dc54881e7e86403efb56ee258229d6d
Truong Giang Do <giangh14cqt@gmail.com>
2023-03-07 19:55:56 +0100

    Add final task 1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ cs1 = ColorSensor(INPUT_1)
 
 tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
 
-squareTurn = 5
+squareTurn = 6
 rollingTime = 0.2
 
 
@@ -22,21 +22,21 @@ while True:
     # print(avg_left, avg_right)
     if avg_right > avg_left:
         tank_drive.on_for_seconds(
-            SpeedPercent(-10), SpeedPercent(-25), rollingTime)  # turn left
+            SpeedPercent(-5), SpeedPercent(-25), rollingTime)  # turn left
     else:
         tank_drive.on_for_seconds(
-            SpeedPercent(-25), SpeedPercent(-10), rollingTime)  # turn right
+            SpeedPercent(-25), SpeedPercent(-5), rollingTime)  # turn right
     if min(getColor()) > 130:
         cnt = 0
         while min(getColor()) > 130 and cnt < squareTurn:
             tank_drive.on_for_seconds(SpeedPercent(
-                10), SpeedPercent(-20), rollingTime)  # turn left
+                17), SpeedPercent(-20), rollingTime)  # turn left
             cnt += 1
 
         cnt = 0
         while min(getColor()) > 130 and cnt < 2*squareTurn:
             tank_drive.on_for_seconds(
-                SpeedPercent(-20), SpeedPercent(10), rollingTime)  # turn right
+                SpeedPercent(-20), SpeedPercent(17), rollingTime)  # turn right
             cnt += 1
 
     # sleep(0.5)
