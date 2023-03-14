413d826e69bc919b200ec1c340da6622264d92d9
Truong Giang Do <giangh14cqt@gmail.com>
2023-03-14 18:45:33 +0100

    Add code fix

--- main_lab1.py
+++ main_lab1.py
@@ -1,7 +1,7 @@
 from ev3dev2.sensor import INPUT_2, INPUT_1
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
 from ev3dev2.sensor.lego import ColorSensor
-from time import sleep
+from datetime import datetime
 
 cs2 = ColorSensor(INPUT_2)
 cs1 = ColorSensor(INPUT_1)
@@ -23,25 +23,32 @@ while True:
 
 
     if abs(avg_right - avg_left) <= 30:
-        tank_drive.on_for_seconds(
-            SpeedPercent(-15), SpeedPercent(-15), rollingTime)  # go straigh
+        tank_drive.on(-15, -15)
+        # tank_drive.on_for_seconds(
+        #     SpeedPercent(-15), SpeedPercent(-15), rollingTime)  # go straigh
     elif avg_right > avg_left:
-        tank_drive.on_for_seconds(
-            SpeedPercent(-5), SpeedPercent(-25), rollingTime)  # turn left
+        tank_drive.on(-5, -25)
+        # tank_drive.on_for_seconds(
+        #     SpeedPercent(-5), SpeedPercent(-25), rollingTime)  # turn left
     else:
-        tank_drive.on_for_seconds(
-            SpeedPercent(-25), SpeedPercent(-5), rollingTime)  # turn right
+        tank_drive.on(-25, -5)
+        # tank_drive.on_for_seconds(
+        #     SpeedPercent(-25), SpeedPercent(-5), rollingTime)  # turn right
     if min(getColor()) > 130:
         cnt = 0
-        while min(getColor()) > 130 and cnt < squareTurn:
-            tank_drive.on_for_seconds(SpeedPercent(
-                17), SpeedPercent(-20), rollingTime)  # turn left
+        start = datetime.now
+        while min(getColor()) > 130 and datetime.now - start <= 3:
+            tank_drive.on(17, -20)
+            # tank_drive.on_for_seconds(SpeedPercent(
+            #     17), SpeedPercent(-20), rollingTime)  # turn left
             cnt += 1
 
         cnt = 0
-        while min(getColor()) > 130 and cnt < 2*squareTurn:
-            tank_drive.on_for_seconds(
-                SpeedPercent(-20), SpeedPercent(17), rollingTime)  # turn right
+        start = datetime.now
+        while min(getColor()) > 130 and datetime.now - start <= 6:
+            tank_drive.on(17, -20)
+            # tank_drive.on_for_seconds(
+            #     SpeedPercent(-20), SpeedPercent(17), rollingTime)  # turn right
             cnt += 1
 
     # sleep(0.5)
