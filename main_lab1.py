c2f262177e3987a02f20b735d12149cf43288fff
Truong Giang Do <giangh14cqt@gmail.com>
2023-03-14 19:57:01 +0100

    add code lab3

--- main_lab1.py
+++ main_lab1.py
@@ -16,39 +16,39 @@ def getColor():
     return sum(cs1.raw)/3, sum(cs2.raw)/3
 
 
+print("start")
 while True:
     avg_left = sum(cs1.raw) / 3
     avg_right = sum(cs2.raw) / 3
     # print(avg_left, avg_right)
 
-
-    if abs(avg_right - avg_left) <= 30:
+    if abs(avg_right - avg_left) <= 60:
         tank_drive.on(-15, -15)
         # tank_drive.on_for_seconds(
         #     SpeedPercent(-15), SpeedPercent(-15), rollingTime)  # go straigh
     elif avg_right > avg_left:
-        tank_drive.on(-5, -25)
+        tank_drive.on(20, -5)
         # tank_drive.on_for_seconds(
         #     SpeedPercent(-5), SpeedPercent(-25), rollingTime)  # turn left
     else:
-        tank_drive.on(-25, -5)
+        tank_drive.on(-5, 20)
         # tank_drive.on_for_seconds(
         #     SpeedPercent(-25), SpeedPercent(-5), rollingTime)  # turn right
-    if min(getColor()) > 130:
-        cnt = 0
-        start = datetime.now
-        while min(getColor()) > 130 and datetime.now - start <= 3:
-            tank_drive.on(17, -20)
-            # tank_drive.on_for_seconds(SpeedPercent(
-            #     17), SpeedPercent(-20), rollingTime)  # turn left
-            cnt += 1
+    # if min(getColor()) > 130:
+    #     cnt = 0
+    #     start = datetime.now()
+    #     while min(getColor()) > 130 and datetime.now() - start <= 3:
+    #         tank_drive.on(17, -20)
+    #         # tank_drive.on_for_seconds(SpeedPercent(
+    #         #     17), SpeedPercent(-20), rollingTime)  # turn left
+    #         cnt += 1
 
-        cnt = 0
-        start = datetime.now
-        while min(getColor()) > 130 and datetime.now - start <= 6:
-            tank_drive.on(17, -20)
-            # tank_drive.on_for_seconds(
-            #     SpeedPercent(-20), SpeedPercent(17), rollingTime)  # turn right
-            cnt += 1
+    #     cnt = 0
+    #     start = datetime.now()
+    #     while min(getColor()) > 130 and datetime.now() - start <= 6:
+    #         tank_drive.on(17, -20)
+    #         # tank_drive.on_for_seconds(
+    #         #     SpeedPercent(-20), SpeedPercent(17), rollingTime)  # turn right
+    #         cnt += 1
 
     # sleep(0.5)
--- main_v2.py
+++ main_v2.py
@@ -9,19 +9,20 @@ YELLOW = [5, 5, 5]
 RED = [5, 5, 5]
 GREEN = [5, 5, 5]
 
-cs1 = ColorSensor(INPUT_1)
-cs2 = ColorSensor(INPUT_2)
+cs1 = ColorSensor(INPUT_2)
+cs2 = ColorSensor(INPUT_1)
 tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
 
 # log = Logger()
-INITIAL_RIGHT_SPEED = 15
-INITIAL_LEFT_SPEED = 15
+INITIAL_RIGHT_SPEED = 7
+INITIAL_LEFT_SPEED = 7
 MIN_SPEED = -5
-MAX_SPEED = 20
-STEP = 0.2
+MAX_SPEED = 10
+STEP = 4
 
 right_speed = INITIAL_RIGHT_SPEED
 left_speed = INITIAL_LEFT_SPEED
+
 
 # class Logger:
 
@@ -40,13 +41,16 @@ def get_color_sensor_1():
     # LEFT
     return cs1.rgb
 
+
 def get_color_sensor_2():
     # RIGHT
     return cs2.rgb
 
+
 def get_color_sensors():
     return (get_color_sensor_1(), get_color_sensor_2())
-    
+
+
 def check_zone():
     sensors = get_color_sensors()
     if sensors == (RED, RED):
@@ -57,29 +61,58 @@ def check_zone():
         print("GREEN Zone")
 
 
+# def correct_speed():
+#     global left_speed
+#     global right_speed
+#     if sum(get_color_sensor_2()) / 3 > sum(get_color_sensor_1()) / 3:
+#         if left_speed < right_speed:
+#             left_speed = INITIAL_LEFT_SPEED
+#             right_speed = INITIAL_RIGHT_SPEED
+#         else:
+#             right_speed = max(MIN_SPEED, right_speed - STEP)
+#             left_speed = min(MAX_SPEED, left_speed + STEP)
+#     else:
+#         if left_speed > right_speed:
+#             left_speed = INITIAL_LEFT_SPEED
+#             right_speed = INITIAL_RIGHT_SPEED
+#         else:
+#             right_speed = min(MAX_SPEED, right_speed + STEP)
+#             left_speed = max(MIN_SPEED, left_speed - STEP)
+
 def correct_speed():
     global left_speed
     global right_speed
-    if sum(get_color_sensor_2()) / 3 > sum(get_color_sensor_1()) / 3:
+    if sum(get_color_sensor_1()) / 3 < 50:
         if left_speed < right_speed:
-            left_speed = INITIAL_LEFT_SPEED
+            left_speed = INITIAL_LEFT_SPEED - 3
             right_speed = INITIAL_RIGHT_SPEED
         else:
             right_speed = max(MIN_SPEED, right_speed - STEP)
             left_speed = min(MAX_SPEED, left_speed + STEP)
-    else:
+    if sum(get_color_sensor_2()) / 3 < 50:
         if left_speed > right_speed:
             left_speed = INITIAL_LEFT_SPEED
-            right_speed = INITIAL_RIGHT_SPEED
+            right_speed = INITIAL_RIGHT_SPEED - 3
         else:
             right_speed = min(MAX_SPEED, right_speed + STEP)
             left_speed = max(MIN_SPEED, left_speed - STEP)
 
+# def correct_speed():
+#     global left_speed
+#     global right_speed
+#     left_speed = -10
+#     right_speed = -10
+#     sleep(3)
+#     left_speed = 5
+#     right_speed = -10
+#     sleep(3)
+#     left_speed = -10
+#     right_speed = 5
 
 
 print("start")
 while True:
-    tank_drive.on(left_speed, right_speed)
+    tank_drive.on(-left_speed, -right_speed)
+    print((sum(get_color_sensor_2()) / 3), sum(get_color_sensor_1()) / 3)
     correct_speed()
     check_zone()
-    
