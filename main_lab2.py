e3e920ae02bbea99f0a0baad686c18683116ae0a
AndriiDemk <96596913+AndriiDemk@users.noreply.github.com>
2023-03-28 16:26:53 +0200

    code refactoring

--- main_lab2.py
+++ main_lab2.py
@@ -17,11 +17,92 @@ squareTurn = 6
 rollingTime = 0.2
 
 
+class TaskExecution:
+
+    def __init__(self, mission, col_sens_1, col_sens_2, parcel_zone_color, delivery_zone_color, path_to_parcel_color, path_to_delivery_color):
+        self.pick_up_status = False
+        self.current_mission = mission
+        self.col_sens_1 = col_sens_1
+        self.col_sens_2 = col_sens_2
+        self.parcel_zone_color = parcel_zone_color
+        self.delivery_zone_color = delivery_zone_color
+        self.path_to_parcel_color = path_to_parcel_color
+        self.path_to_delivery_color = path_to_delivery_color
+
+    def normal_running(self):
+        _, avg_left = self.col_sens_1
+        _, avg_right = self.col_sens_2
+        if abs(avg_right - avg_left) <= 60:
+            tank_drive.on(-15, -15)
+        elif avg_right > avg_left:
+            tank_drive.on(20, -5)
+        else:
+            tank_drive.on(-5, 20)
+
+    def correct_picking_position(self):
+        print("Correct me!")
+
+    def pick_up(self):
+        print("Picking up!")
+        if distance.proximity != None and distance.proximity <= 2:
+            my_motor.on_for_seconds(SpeedRPM(15), 1)
+            self.current_mission = Mission.RETURN_TO_PATH
+        else:
+            self.correct_picking_position()
+
+        # tank_drive.on_for_seconds(
+        #     SpeedPercent(30), SpeedPercent(-30), 1)  # turn back
+
+        # tank_drive.on_for_seconds(
+        #     SpeedPercent(-20), SpeedPercent(-20), 1)  # turn back
+
+    def delivery():
+        print("Delivery!")
+        my_motor.on_for_seconds(SpeedRPM(-15), 1)
+
+        tank_drive.on_for_seconds(
+            SpeedPercent(50), SpeedPercent(50), 1)  # turn back
+        tank_drive.on_for_seconds(
+            SpeedPercent(50), SpeedPercent(-50), 5)  # cheer
+
+    def event_check(self):
+        ccs1, avg_left = self.col_sens_1
+        ccs2, avg_right = self.col_sens_2
+        if self.current_mission == Mission.PATH_TO_PARCEL and \
+                (ccs1 == self.path_to_parcel_color or ccs2 == self.path_to_parcel_color):
+            self.current_mission = Mission.FROM_PATH_TO_PARCEL
+        if self.current_mission == Mission.FROM_PATH_TO_PARCEL and \
+                (ccs1 == self.parcel_zone_color and ccs2 == self.parcel_zone_color):
+            self.current_mission = Mission.PICK_UP
+
+    def update_data(self, col_sens_1, col_sens_2):
+        self.col_sens_1 = col_sens_1
+        self.col_sens_2 = col_sens_2
+        self.event_check()
+
+    def run_events(self):
+        if self.current_mission is Mission.PATH_TO_PARCEL:
+            self.normal_running()
+        if self.current_mission is Mission.PICK_UP:
+            self.pick_up()
+
+
 class Color(Enum):
     RED = 1
     GREEN = 2
     BLUE = 3
     WHITE = 4
+
+
+class Mission(Enum):
+    PICK_UP = 1  # pick up the parcel with medium motor only
+    DELIVERY = 2
+    # line following until find spesial color(RED GREEN) with ONE of the sensors
+    PATH_TO_PARCEL = 3
+    # line following until find spesial color(RED GREEN) with TWO of the sensors
+    FROM_PATH_TO_PARCEL = 4
+    # line following until find spesial color(RED GREEN)depends which one was first
+    RETURN_TO_PATH = 3
 
 
 def check_color(cs):
@@ -38,125 +119,26 @@ def check_color(cs):
     return None, (r+g+b)/3
 
 
-def getColor():
-    return sum(cs1.raw)/3, sum(cs2.raw)/3
-
-
-def normal_running(priority=None):
-    # avg_left = sum(cs1.raw) / 3
-    # avg_right = sum(cs2.raw) / 3
-
-    # if abs(avg_right - avg_left) <= 30:
-    #     tank_drive.on_for_seconds(
-    #         SpeedPercent(-15), SpeedPercent(-15), rollingTime)  # go straigh
-    # elif avg_right > avg_left or check_color(cs1) == priority:
-    #     tank_drive.on_for_seconds(
-    #         SpeedPercent(-5), SpeedPercent(-25), rollingTime)  # turn left
-    # else:
-    #     tank_drive.on_for_seconds(
-    #         SpeedPercent(-25), SpeedPercent(-5), rollingTime)  # turn right
-    # if min(getColor()) > 130:
-    #     cnt = 0
-    #     while min(getColor()) > 130 and cnt < squareTurn:
-    #         tank_drive.on_for_seconds(SpeedPercent(
-    #             17), SpeedPercent(-20), rollingTime)  # turn left
-    #         cnt += 1
-
-    #     cnt = 0
-    #     while min(getColor()) > 130 and cnt < 2*squareTurn:
-    #         tank_drive.on_for_seconds(
-    #             SpeedPercent(-20), SpeedPercent(17), rollingTime)  # turn right
-    #         cnt += 1
-    # avg_left = sum(cs1.raw) / 3
-    # avg_right = sum(cs2.raw) / 3
-    
-    ccs1, avg_left = check_color(cs1)
-    ccs2, avg_right = check_color(cs2)
-    # print(check_color(cs1), check_color(cs2))
-
-    if ccs1 == priority:
-        tank_drive.on(20, -5)
-    elif ccs2 == priority:
-        tank_drive.on(-5, 20)
-    elif abs(avg_right - avg_left) <= 60:
-        tank_drive.on(-15, -15)
-    elif avg_right > avg_left:
-        tank_drive.on(20, -5)
-    else:
-        tank_drive.on(-5, 20)
-
-
-class Mission(Enum):
-    PICK_UP = 1 # pick up the parcel with medium motor only
-    DELIVERY = 2 # 
-    PATH_TO_PARCEL = 3 # line following until find spesial color(RED GREEN) with ONE of the sensors
-    FROM_PATH_TO_PARCEL = 4 # line following until find spesial color(RED GREEN) with TWO of the sensors
-    RETURN_TO_PATH = 3 # line following until find spesial color(RED GREEN)depends which one was first
-
-
-
-
-def pick_up():
-    print("Picking up!")
-    my_motor.on_for_seconds(SpeedRPM(15), 1)
-    
-    tank_drive.on_for_seconds(
-        SpeedPercent(30), SpeedPercent(-30), 1)  # turn back
-
-    tank_drive.on_for_seconds(
-        SpeedPercent(-20), SpeedPercent(-20), 1)  # turn back
-
-
-def delivery():
-    print("Delivery!")
-    my_motor.on_for_seconds(SpeedRPM(-15), 1)
-    
-    tank_drive.on_for_seconds(
-        SpeedPercent(50), SpeedPercent(50), 1)  # turn back
-    tank_drive.on_for_seconds(
-        SpeedPercent(50), SpeedPercent(-50), 5)  # cheer
-
-def run_loop(pick_up_color, delivery_color, mission, priority):
+def run_loop(tasks: TaskExecution):
     while (True):
         ccs1, rgbcs1 = check_color(cs1)
         ccs2, rgbcs2 = check_color(cs2)
-        if mission is Mission.PATH_TO_PARCEL:
-            normal_running(priority)
-        elif mission is Mission.FROM_PATH_TO_PARCEL:
-            m
+        tasks.update_status((ccs1, rgbcs1), (ccs2, rgbcs2))
+        tasks.run_events()
 
-
-        normal_running(priority)
-        # if mission is Mission.PICK_UP:
-        #     priority = Color.RED
-        if mission is Mission.PICK_UP:
-            # pass
-            print(distance.proximity)
-            ccs1, _ = check_color(cs1)
-            ccs2, _ = check_color(cs2)
-            if distance.proximity != None and distance.proximity <= 2 \
-                    and ccs1 == pick_up_color \
-                    and ccs2 == pick_up_color:
-                pick_up()
-                priority = delivery_color
-                mission = Mission.DELIVERY
-        else:
-            if ccs1 == delivery_color \
-                    and ccs2 == delivery_color:
-                delivery()
-                print("Deliveried!")
-                exit()
 
 def main():
-    # while True:
-    #     pick_up()
-    #     delivery()
-    pick_up_color = Color.RED
-    delivery_color = Color.GREEN
-    mission = Mission.PICK_UP
-    priority = pick_up_color
-    run_loop(pick_up_color, delivery_color, mission, priority)
-    
+    parcel_zone_color = Color.GREEN
+    delivery_zone_color = Color.BLUE
+    path_to_parcel_color = Color.RED
+    path_to_delivery_color = Color.BLUE
+    tasks = TaskExecution(mission=Mission.PATH_TO_PARCEL,
+                          col_sens_1=None, col_sens_2=None,
+                          parcel_zone_color=parcel_zone_color,
+                          delivery_zone_color=delivery_zone_color,
+                          path_to_parcel_color=path_to_parcel_color,
+                          path_to_delivery_color=path_to_delivery_color,)
+    run_loop(tasks)
 
 
 if __name__ == '__main__':
